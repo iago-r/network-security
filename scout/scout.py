de2ff679032e656ac12652a517e19febd49a2e61
Lucas <sacramento.15@hotmail.com>
2023-06-13 16:00:34 -0300

    made pull request fixes

--- scout/scout.py
+++ scout/scout.py
@@ -15,15 +15,16 @@ from typing import Any, Callable, Protocol
 
 import docker
 
-from typing import List
 
 DOCKER_STATUSCODE_KEY = "StatusCode"
 OUTDIR_CONTAINER_MOUNT = "/root/output"
 SCOUT_TASK_LABEL_KEY = "scout-task-id"
 
 
-# # https://docs.docker.com/engine/reference/commandline/ps/
-class ContainerState(enum.Enum):
+# https://docs.docker.com/engine/reference/commandline/ps/
+class ContainerState(
+    enum.Enum
+):
     CREATED = "created"
     RESTARTING = "restarting"
     RUNNING = "running"
@@ -35,29 +36,16 @@ class ContainerState(enum.Enum):
     def is_done(self):
         return self in [ContainerState.EXITED, ContainerState.DEAD]
 
-# Set de configuração do scout especificamente
-# @dataclasses.dataclass(frozen=True)
-# class ScoutConfig:
-#     credentials_file: pathlib.Path
-#     output_dir: pathlib.Path
-#     docker_image: str = "rossja/ncc-scoutsuite:aws-latest"
-#     docker_poll_interval: float = 16.0
-#     docker_socket: str | None = None
-#     docker_timeout: int = 5
-    # definir os comandos da imagem para o alpine
-
-# commands scout - usado para rodar o modulo
-# commands_scout = ["scout","aws","--no-browser","--result-format","json","--report-dir",f"{OUTDIR_CONTAINER_MOUNT}","--logfile",f"{OUTDIR_CONTAINER_MOUNT}/scout.log",]
-# volumes_scout = {str(self.config.credentials_file): {"bind": "/root/.aws/credentials","mode": "ro",},str(outfp): {"bind": OUTDIR_CONTAINER_MOUNT,"mode": "rw",},}
 
 @dataclasses.dataclass(frozen=True)
 class ScoutConfig:
     credentials_file: pathlib.Path
     output_dir: pathlib.Path
-    docker_image: str = "alpine"
+    docker_image: str
     docker_poll_interval: float = 16.0
     docker_socket: str | None = None
     docker_timeout: int = 5
+
 
 class Task(Protocol):
     label: str
@@ -66,6 +54,8 @@ class Task(Protocol):
 @dataclasses.dataclass(frozen=True)
 class ScoutTask:
     label: str
+    commands: str | None = None 
+    volumes: dict | None = None 
     aws_api_key: str | None = None
     aws_api_secret: str | None = None
     role_arn: str | None = None
@@ -77,10 +67,14 @@ TaskCompletionCallback = Callable[[str, bool], None]
 class ScanModule(Protocol):
     def __init__(self, config: Any, callback: TaskCompletionCallback) -> None:
         ...
+
     def enqueue(self, taskcfg: Task) -> None:
         ...
+
     def shutdown(self, wait: bool) -> None:
         ...
+
+
 
 class Scout(ScanModule):
     def __init__(self, config: ScoutConfig, callback: TaskCompletionCallback) -> None:
@@ -95,30 +89,26 @@ class Scout(ScanModule):
         self.containers: set = set()
         self.task_completion_callback = callback
         self.lock: threading.Lock = threading.Lock()
-        self.thread: threading.Thread = threading.Thread(target=self.scout_polling_thread,name="scout-polling-thread",)
+        self.thread: threading.Thread = threading.Thread(
+            target=self.scout_polling_thread,
+            name="scout-polling-thread",
+        )
         self.thread.start()
 
-    def enqueue(self, taskcfg: Task, commands: List[str], time: int = 0) -> None:
+    def enqueue(self, taskcfg: Task) -> None:
         assert isinstance(taskcfg, ScoutTask)
-
-        if(time != 0):
-            commands[1] = f"{time}"
 
         outfp = self.config.output_dir / taskcfg.label
         os.makedirs(outfp, exist_ok=True)
         try:
             ctx = self.docker.containers.run(
                 self.config.docker_image,
-                
-                command=commands,
-
+                command=taskcfg.commands,
                 detach=True,
                 labels={SCOUT_TASK_LABEL_KEY: taskcfg.label},
                 stdout=True,
                 stderr=True,
-                volumes={
-                    #####
-                },
+                volumes=taskcfg.volumes,
                 working_dir="/root",
             )
         except docker.errors.APIError as e:
@@ -140,7 +130,7 @@ class Scout(ScanModule):
         else:
             self.handle_finished_containers()
             self.thread.join()
-            
+
         logging.info("Joined Scout polling thread, module shut down")
 
     def scout_polling_thread(self) -> None:
@@ -163,27 +153,32 @@ class Scout(ScanModule):
                     continue
 
                 assert cfg.label == ctx.labels[SCOUT_TASK_LABEL_KEY]
-                
+
                 r = ctx.wait(timeout=self.config.docker_timeout)
-                r["stdout"] = ctx.logs(stdout=True, stderr=False, timestamps=True).decode("utf8")
-                r["stderr"] = ctx.logs(stdout=False, stderr=True, timestamps=True).decode("utf8")
-                
+                r["stdout"] = ctx.logs(
+                    stdout=True, stderr=False, timestamps=True
+                ).decode("utf8")
+                r["stderr"] = ctx.logs(
+                    stdout=False, stderr=True, timestamps=True
+                ).decode("utf8")
+
                 outfp = self.config.output_dir / cfg.label
                 with gzip.open(outfp / "result.json.gz", "wt", encoding="utf8") as fd:
                     json.dump(r, fd)
                 self.task_completion_callback(cfg.label, True)
-                logging.info("Scout run completed, id %s sta+-tus %d",
+                logging.info(
+                    "Scout run completed, id %s sta+-tus %d",
                     cfg.label,
                     r[DOCKER_STATUSCODE_KEY],
                 )
                 completed.add((ctx, cfg))
 
             self.containers -= completed
-            
+
             logging.info(
                 "Running %d ScoutSuite containers, waiting %d seconds to refresh",
                 len(self.containers),
-                self.config.docker_poll_interval
+                self.config.docker_poll_interval,
             )
 
         for ctx, _cfg in completed:
--- scout/tests/testrun.py
+++ scout/tests/testrun.py
@@ -11,9 +11,29 @@ import scout
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t1', '--task1', type=int ,help='Task 1')
-parser.add_argument('-t2', '--task2', type=int , help='Task 2')
-parser.add_argument('-t3', '--task3', type=int , help='Task 3')
+parser.add_argument(
+    "-t1", "--test1", type=int, help="Test 1: Container removed after n seconds."
+)
+parser.add_argument(
+    "-t2",
+    "--test2",
+    type=int,
+    help="Test 2: Container with infinite execution removed after n seconds.",
+)
+parser.add_argument(
+    "-t3",
+    "--test3",
+    type=int,
+    help="Test 3: Container removed after returning an error.",
+)
+
+parser.add_argument(
+    "-scout",
+    "--scout_suite",
+    action='store_true',
+    help="SCOUT: Running the Scout module.",
+)
+
 args = parser.parse_args()
 
 
@@ -22,34 +42,90 @@ assert pathlib.Path(CWD / __file__).exists(), "Run from inside tests/ with ./tes
 SCOUT_MOD_PATH = CWD.parent
 
 
+DOCKER_STATUSCODE_KEY = "StatusCode"
+OUTDIR_CONTAINER_MOUNT = "/root/output"
+SCOUT_TASK_LABEL_KEY = "scout-task-id"
+
+
 def callback(label: str, success: bool) -> None:
     logging.info("Callback for %s running, success=%s", label, success)
 
-def task_1(s, taskcfg, seconds: int):
-    task_generic_sleep = ["sleep", f'{1}']
-    logging.info("")
-    logging.info("RUNNING TASK 1")
-    s.enqueue(taskcfg, task_generic_sleep, seconds)
+
+def test_scout(s):
+    taskcfg = scout.ScoutTask(
+        time.strftime("scout-%Y%m%d-%H%M%S"),
+        f"scout aws --no-browser --result-format json \
+        --report-dir {OUTDIR_CONTAINER_MOUNT} --logfile \
+        {OUTDIR_CONTAINER_MOUNT}/scout.log",    # Commands_scout
+        
+        volumes = {
+            './dev/aws-credentials': {
+                'bind': '/root/.aws/credentials',
+                'mode': 'ro'
+            },
+            './data': {
+                'bind': '/root/output',
+                'mode': 'rw'
+            }
+        } # volumes_scout
+
+    )
+
+    logging.info("RUNNING TEST SCOUT")
+    s.enqueue(taskcfg)
     logging.info("Task submitted")
     s.shutdown()
 
-def task_2(s, taskcfg, seconds: int):
-    task_generic_inf = ["tail", "-f" ,"/dev/null"]
-    logging.info("")
-    logging.info("RUNNING TASK 2")
-    s.enqueue(taskcfg, task_generic_inf)
+
+def task_1(s, seconds: int):
+    taskcfg = scout.ScoutTask(
+        time.strftime("scout-%Y%m%d-%H%M%S"),
+        f"sleep {seconds}",  # commands
+    )
+
+    taskcfg2 = scout.ScoutTask(
+        time.strftime("scout-%Y%m%d-%H%M%S"),
+        f"sleep 5"
+    )
+
+    logging.info("RUNNING TEST 1 - The container was removed after n seconds, along with another set of tasks.")
+    s.enqueue(taskcfg)
+    s.enqueue(taskcfg2)
+    logging.info("Task submitted")
+    s.shutdown()
+
+
+def task_2(s, seconds: int):
+    taskcfg = scout.ScoutTask(
+        time.strftime("scout-%Y%m%d-%H%M%S"),
+        "tail -f /dev/null",  # commands
+    )
+
+    logging.info(
+        "RUNNING TEST 2 - Container with infinite execution removed after n seconds."
+    )
+    s.enqueue(taskcfg)
+    
     time.sleep(seconds)
     logging.info("Task submitted")
     s.shutdown(False)
 
-def task_3(s, taskcfg, seconds: int):
-    task_generic_error = ["sh", "-c", f'sleep {seconds}', "&&" , "exit", "1"]
-    logging.info("")
-    logging.info("RUNNING TASK 3")
-    s.enqueue(taskcfg, task_generic_error)
+
+def task_3(s, seconds: int):
+    taskcfg = scout.ScoutTask(
+        time.strftime("scout-%Y%m%d-%H%M%S"),
+        f"sh -c 'sleep {seconds} && exit 1'",
+    )
+
+    logging.info("RUNNING TEST 3 - Container removed after returning an error.")
+    s.enqueue(taskcfg)
+    
     logging.info("Task submitted")
     s.shutdown()
 
+
+scout_image = "rossja/ncc-scoutsuite:aws-latest"
+generic_image = "alpine"
 
 
 def main():
@@ -58,29 +134,31 @@ def main():
     cfg = scout.ScoutConfig(
         pathlib.Path(SCOUT_MOD_PATH / "dev/aws-credentials"),
         pathlib.Path(SCOUT_MOD_PATH / "data"),
+        docker_image=scout_image,
         docker_poll_interval=1.0,
     )
     logging.info("Scout module started")
     s = scout.Scout(cfg, callback)
     logging.info("Submitting task to Scout module")
-    taskcfg = scout.ScoutTask(
-        time.strftime("scout-%Y%m%d-%H%M%S"),
-    )
- 
-    #python3 testrun.py -t1 "tempo de execução em segundos"
-    if args.task1:
-        seconds = args.task1
-        task_1(s, taskcfg, seconds)
 
-    #python3 testrun.py -t2 "tempo de execução em segundos"
-    if args.task2:
-        seconds = args.task2
-        task_2(s, taskcfg, seconds)
+    # python3 testrun.py -t1 "tempo de execução em segundos"
+    if args.test1:
+        seconds = args.test1
+        task_1(s, seconds)
 
-    #python3 testrun.py -t3 "tempo de execução em segundos"
-    if args.task3:
-        seconds = args.task3
-        task_3(s, taskcfg, seconds)
+    # #python3 testrun.py -t2 "tempo de execução em segundos"
+    if args.test2:
+        seconds = args.test2
+        task_2(s, seconds)
+
+    # python3 testrun.py -t3 "tempo de execução em segundos"
+    if args.test3:
+        seconds = args.test3
+        task_3(s, seconds)
+
+    #python3 testrun.py -scout
+    if args.scout_suite:            
+        test_scout(s)
 
 
 if __name__ == "__main__":
