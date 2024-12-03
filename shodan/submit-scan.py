8b499c4a46ee38f1e75a037cf43aa7329cde34bc
Italo Cunha <cunha@dcc.ufmg.br>
2024-12-03 20:20:34 -0300

    fix: update info

--- shodan/submit-scan.py
+++ shodan/submit-scan.py
@@ -3,7 +3,7 @@
 from shodan import Shodan
 
 # Setup the Shodan API object
-api = Shodan("23JnVloslnwzHEWAKzTXU2VEG27xCioo")
+api = Shodan("load_api_here")
 
 # Submit a scan request for 1 IP and 1 network range
 scan = scan = api.scan({
@@ -13,5 +13,3 @@ scan = scan = api.scan({
 })
 
 print(scan)
-# shodan download --limit 100 scan-cecremge-5000https.json.gz scan:uwOh9JWw5peV5TLX
-# {'count': 1, 'id': 'uwOh9JWw5peV5TLX', 'credits_left': 5053}
