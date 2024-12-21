5b6805b02cff35b2e06d5ddc05a1fb893dd52d02
Italo Cunha <cunha@dcc.ufmg.br>
2024-12-21 11:57:59 -0300

    fix: improve Shodan scripts

--- shodan/submit-scan.py
+++ shodan/submit-scan.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
 
+import os
+import sys
+
 from shodan import Shodan
 
+TARGETS_FILE = "hostnames.txt"
+
+shodan_api_key = os.getenv("shodan_api_key")
+if shodan_api_key is None:
+    print("Please set the 'shodan_api_key' env variable")
+    sys.exit(1)
+
 # Setup the Shodan API object
-api = Shodan("23JnVloslnwzHEWAKzTXU2VEG27xCioo")
+api = Shodan(shodan_api_key)
 
 # Submit a scan request for 1 IP and 1 network range
-scan = scan = api.scan({
-    '200.233.189.132': [
-        (5000, 'https'),
-    ]
-})
+# docs: https://shodan.readthedocs.io/en/latest/api.html
 
+with open(TARGETS_FILE, encoding="utf8") as fd:
+    ips = [line.strip() for line in fd]
+
+scan = api.scan(ips)
 print(scan)
 # shodan download --limit 100 scan-cecremge-5000https.json.gz scan:uwOh9JWw5peV5TLX
 # {'count': 1, 'id': 'uwOh9JWw5peV5TLX', 'credits_left': 5053}
